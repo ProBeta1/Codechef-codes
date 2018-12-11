30d8fd4757f043b7f495a4790788df5976d81675
Abhinav Kumar Srivastava <33081197+ProBeta1@users.noreply.github.com>
2018-12-11 09:53:07 +0530

    Create c01.cpp

---
+++ Challenge/Nov 18/c01.cpp
@@ -0,0 +1,21 @@
+
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+#define mod 1000000007
+int main()
+{
+	#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+	#endif
+	int t,n,x,i,j;
+	ll a,b;
+	cin>>t;
+	while(t--)
+	{
+		cin>>a>>b;
+		cout<<"YES"<<endl;
+	}	
+
+}
